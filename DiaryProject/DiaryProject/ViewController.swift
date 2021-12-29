b7b308e314f14e7988ad7a6bc868e4547c07b23e
ryuchanghwi <ckdgnl10@naver.com>
2021-12-29 21:07:26 +0900

    Diary

---
+++ DiaryProject/DiaryProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiaryProject
+//
+//  Created by 류창휘 on 2021/12/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
